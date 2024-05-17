bd20711afa9c7814b29778e8096e59525326a0b4
Mikicrep <mikicrepoffical@gmail.com>
2024-05-17 17:32:27 +0200

    Added title

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 
 #include "utilities.h"
 
@@ -10,6 +11,10 @@ int main()
 	while(running) {
 		std::cout << "\033[2J\033[1;1H"; // Clear terminal after input
 
+		time_t time = std::time(0);
+		char* timetext = std::ctime(&time);
+
+		std::cout << "Mikibox | " << timetext << "----------------\n";
 		std::cout << "Write number to select thing that will run or write 0 to exit:\n"
 << "1. Calculator\n"
 << "2. Random number generator\n";
